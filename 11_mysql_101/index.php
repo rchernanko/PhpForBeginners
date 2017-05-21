b38c299c89ee29d1b8cc3436fa12e9f7d85890ab
Richard Chernanko <rchernanko@hotmail.co.uk>
2017-05-21 17:55:46 +0100

    adding some comments re: mysql lesson

---
+++ 11_mysql_101/index.php
@@ -0,0 +1,6 @@
+<?php
+
+//Have created a database called laracasts
+
+//create table todos (id int primary key auto_increment, description text not null, completed boolean not null);
+//insert into todos (description, completed) values ('Go to the store', false)
